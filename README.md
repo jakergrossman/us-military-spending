f041394bbe5b59df84ef2d0dbe994a8e764fe76b
jakergrossman <jake.r.grossman@gmail.com>
2022-02-20 18:23:58 -0600

    feat: Add support for multiple budget sources

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # us-military-spending
 Shows estimated United States Department of Defense spending since the start of the year, day, and minute.
 
-Uses data from the [National Defense Budget Estimate](https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2020/FY20_Green_Book.pdf) for FY 2020
+Supports using data from the National Defense Budget Estimate for FY 2020, FY 2021, and FY 2022.
--- app.js
+++ app.js
@@ -1,47 +1,99 @@
-// DoD Budget authority sourced from the Nation Defense Budget Estimates for FY 2020
-// https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2020/FY20_Green_Book.pdf
-const DOD_BUDGET = 718249000000;
+// 2022 Jake Grossman
+// This is free and unencumbered software released into the public domain.
+// See LICENSE.txt for more information.
 
-// sourced from US 2020 census https://data.census.gov/cedsci/table?q=United%20States&tid=DECENNIALPL2020.P1z
-const US_POP = 331449281;
+const BUDGET_TABLE = [
+    // Nation Defense Budget Estimates for FY 2022
+    // https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2022/FY22_Green_Book.pdf
+    //
+    // Accessed February 20, 2022
+    {
+        "source": "National Defense Budge Estimates for FY 2022",
+        "budget": 715000000000
+    },
+    
+    // Nation Defense Budget Estimates for FY 2021
+    // https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2021/FY21_Green_Book.pdf
+    //
+    // Accessed February 20, 2022
+    {
+        "source": "National Defense Budge Estimates for FY 2021",
+        "budget": 705392000000
+    },
+    
+    // Nation Defense Budget Estimates for FY 2020
+    // https://comptroller.defense.gov/Portals/45/Documents/defbudget/fy2020/FY20_Green_Book.pdf
+    //
+    // Accessed February 20, 2022
+    {
+        "source": "National Defense Budge Estimates for FY 2020",
+        "budget": 718349000000
+    },
+];
 
-// 24 Hours * 60 Minutes * 60 Seconds * 1000 Milliseconds
-const MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
-const USD_PER_MILLISECOND = DOD_BUDGET / 365 / MILLIS_PER_DAY;
+// 365 Days * 24 Hours * 60 Minutes * 60 Seconds * 1000 Milliseconds
+const millisPerYear = 365 * 24 * 60 * 60 * 1000;
+
+// index of current budget in BUDGET_TABLE
+var budgetPos = 0;
+
+// number display HTML elements
+var yearDisplay, todayDisplay, minuteDisplay, secondCounter;
+
+// footer information HTML elements
+var sourceElement, budgetElement;
+
+var usdPerMillisecond = 0;
+function setBudget(index) {
+    index = (index % BUDGET_TABLE.length + BUDGET_TABLE.length) % BUDGET_TABLE.length;
+    budgetPos = index;
+    sourceElement.innerText = BUDGET_TABLE[budgetPos]["source"];
+    budgetElement.innerText = "$" + BUDGET_TABLE[budgetPos]["budget"].toLocaleString();
+    usdPerMillisecond = BUDGET_TABLE[budgetPos]["budget"] / millisPerYear;
+}
 
 // calculate USD spent between time a and b
 function usdTime(a, b) {
     let elapsedMillis = b - a;
 
-    return Math.round(elapsedMillis * USD_PER_MILLISECOND);
+    return Math.round(elapsedMillis * usdPerMillisecond);
 }
 
 // update display
 function update() {
     // get time references
-    let now = new Date;
-    let yearStart = new Date(now.getFullYear(), 0, 1);
-    let dayStart = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    let now         = new Date;
+    let yearStart   = new Date(now.getFullYear(), 0, 1);
+    let dayStart    = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     let minuteStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes());
 
-    // get $ amts
-
-    let usdYear = '$' + usdTime(yearStart, now).toLocaleString(); // toLocaleString uses browser locale to format $ amt
-
-    let usdToday = '$' + usdTime(dayStart, now).toLocaleString();
-
+    // get $ amts, using `toLocaleString' in internationalize decimal seperators
+    let usdYear   = '$' + usdTime(yearStart, now).toLocaleString();
+    let usdToday  = '$' + usdTime(dayStart, now).toLocaleString();
     let usdMinute = '$' + usdTime(minuteStart, now).toLocaleString();
 
-    document.querySelector('#year').textContent = usdYear;
-    document.querySelector('#today').textContent = usdToday;
+    yearDisplay.textContent   = usdYear;
+    todayDisplay.textContent  = usdToday;
+    minuteDisplay.textContent = usdMinute;
 
-    // update number of seconds
-    // seconds is 0 indexed
+    // display # of seconds since minute start (0 indexed)
     let seconds = new Date().getSeconds() + 1;
-    document.querySelector('#seconds').textContent = seconds + ' ' + (seconds < 2 ? 'Second' : 'Seconds');
+    secondCounter.textContent = seconds + ' ' + (seconds == 1 ? 'Second' : 'Seconds');
 
-    document.querySelector('#minute').textContent = usdMinute;
 }
 
-// 60x per second
-setInterval(update, 16.6666666667);
+function init() {
+    yearDisplay   = document.querySelector('#year');
+    todayDisplay  = document.querySelector('#today');
+    minuteDisplay = document.querySelector('#minute');
+    secondCounter = document.querySelector('#seconds');
+    sourceElement = document.querySelector('#source');
+    budgetElement = document.querySelector('#budget');
+
+    setBudget(0);
+
+    // 60x second
+    setInterval(update, 16.6666666667);
+}
+
+window.addEventListener('load', init);
